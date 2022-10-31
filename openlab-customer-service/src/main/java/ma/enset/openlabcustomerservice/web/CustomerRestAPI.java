2caf6b5000a55b3d16b0ecfc45175d46622a60bc
Arij Bouhlala <Bouhlalaarij@gmail.com>
2022-10-31 23:45:47 +0100

    Service customer

--- openlab-customer-service/src/main/java/ma/enset/openlabcustomerservice/web/CustomerRestAPI.java
+++ openlab-customer-service/src/main/java/ma/enset/openlabcustomerservice/web/CustomerRestAPI.java
@@ -3,10 +3,7 @@ package ma.enset.openlabcustomerservice.web;
 import ma.enset.openlabcustomerservice.dto.CustomerRequestDTO;
 import ma.enset.openlabcustomerservice.dto.CustomerResponseDTO;
 import ma.enset.openlabcustomerservice.services.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,13 +23,13 @@ public class CustomerRestAPI {
     }
 
     @PostMapping(path="/customers")
-    public CustomerResponseDTO save(CustomerRequestDTO customerRequestDTO){
+    public CustomerResponseDTO save(@RequestBody CustomerRequestDTO customerRequestDTO){
         customerRequestDTO.setId(UUID.randomUUID().toString());
         return customerService.save(customerRequestDTO);
     }
 
     @GetMapping(path="/customers/{id}")
-    public CustomerResponseDTO getCustomer(String id){
+    public CustomerResponseDTO getCustomer(@PathVariable String id){
         return customerService.getCustomer(id);
     }
 }
